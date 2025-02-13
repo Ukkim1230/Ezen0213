79b631b3c412cda48e0d8353d0941a00a97a83b7
Administrator <Administrator@i7E-11>
2025-02-13 09:30:43 +0900

    dd

---
+++ ezen/src/p0213/Action.java
@@ -0,0 +1,7 @@
+package p0213;
+
+public interface Action {//interface의 접근제어자는 public
+	void eat();
+	void play();
+	void work();
+}
---
+++ ezen/src/p0213/InterfaceTest.java
@@ -0,0 +1,9 @@
+package p0213;
+
+//영역이 없어도 됨 오히려 영역설정시 오류
+public class InterfaceTest implements Action {
+
+	public static void main(String[] args) {
+	}
+
+}
