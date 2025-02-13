19a2a56497e6328e68a325cd6ced6c21abb0c430
Administrator <Administrator@i7E-11>
2025-02-13 09:31:50 +0900

    dd

--- ezen/src/p0213/InterfaceTest.java
+++ ezen/src/p0213/InterfaceTest.java
@@ -2,8 +2,16 @@ package p0213;
 
 //영역이 없어도 됨 오히려 영역설정시 오류
 public class InterfaceTest implements Action {
-
-	public static void main(String[] args) {
+	public void eat() {
+		
 	}
-
+	public void play() {
+		
+	}
+	public void work() {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
 }
