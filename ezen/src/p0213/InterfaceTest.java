7777758fa418383210d7acf0a215c5098958a8fc
Administrator <Administrator@i7E-11>
2025-02-13 15:45:00 +0900

    dd

--- ezen/src/p0213/InterfaceTest.java
+++ ezen/src/p0213/InterfaceTest.java
@@ -1,17 +1,25 @@
 package p0213;
 
 //영역이 없어도 됨 오히려 영역설정시 오류
+//메모리 생성 x ex)Action ac = new Action();X
 public class InterfaceTest implements Action {
 	public void eat() {
-		
+		System.out.println("먹는다");
 	}
 	public void play() {
-		
+		System.out.println("논다");
 	}
 	public void work() {
-		
+		System.out.println("일한다");
+	}
+	public void sleep() {
+		System.out.println("잔다");
 	}
 	public static void main(String[] args) {
-		
+		Action a = new InterfaceTest();
+		a.eat();
+		a.play();
+		a.work();
+		//a.sleep();
 	}
 }
